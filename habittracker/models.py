743a77a83aea0fa7b4a4385f36df3691b8e8f391
Philip <philipweiss1260@gmail.com>
2022-05-19 12:42:01 -0400

    models

--- habittracker/models.py
+++ habittracker/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     pass
     # add additional fields in here
